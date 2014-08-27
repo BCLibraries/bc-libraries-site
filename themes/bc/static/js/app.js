bd7fb6894cc001f38356d9de7a27590673f233be
Benjamin Florin <benjamin.florin@gmail.com>
2014-08-27 16:22:02 -0400

    Added basic typeahead structure

---
+++ themes/bc/static/js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+(function () {
+    angular.module('myApp', [
+        'ngRoute',
+        'myApp.controllers',
+        'ui.bootstrap'
+    ]).
+        config(['$routeProvider', function ($routeProvider) {
+            $routeProvider.when('/search', {templateUrl: 'theme/partials/bento.html', controller: 'bento'});
+            $routeProvider.otherwise({templateUrl: 'theme/partials/typeahead.html', controller: 'bento'});
+        }]);
+})();

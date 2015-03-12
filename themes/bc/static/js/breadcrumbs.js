a9a972ef047f298a7f8b98f1cc4b97949f90fedf
Jesse Martinez <mjesse@gmail.com>
2015-03-12 15:44:09 -0400

    Rewrite of breadcrumbs script

--- themes/bc/static/js/breadcrumbs.js
+++ themes/bc/static/js/breadcrumbs.js
@@ -1,30 +1,49 @@
 /* MAKES SUBNAV MENU ITEM ACTIVE & APPEAR IN BREADCRUMBS */
 
-   $(document).ready(function(){
-         var activeLI = $("#s-lg-guide-tabs ul.nav-tabs li.active > a");
-         if (activeLI && activeLI.length > 0) {
-            var parentHREF = $(activeLI).attr("href");
-            var parentText = $(activeLI).text().trim();
-            var loc = $(location).attr('pathname') + $(location).attr('search');
-            loc = loc.replace(/\/libguides\//,"");
+$(document).ready(function(){
+    // check to see if there is an active tab
+    var activeLI = $("#s-lg-guide-tabs ul.nav-tabs li.active > a");
+    if (activeLI && activeLI.length > 0) {
+        // grab the active tab's link and text
+        var parentHREF = $(activeLI).attr("href");
+        var parentText = $(activeLI).text().trim();
+        // get location URL
+        var loc = $(location).attr('pathname') + $(location).attr('search');
+        loc = loc.replace(/\/libguides\//,"").replace(/\?preview.*$/,"");
 
-            if (parentHREF != loc) {
-               var subpageDOM = $(activeLI).siblings("ul.s-lg-subtab-ul");
-               var subpageATag = subpageDOM.find("li a");
+        // if what is set as active doesn't match what we know is the page location
+        if (parentHREF != loc) {
+            // find the subnav menu for the active menu
+            var subpageDOM = $(activeLI).siblings("ul.s-lg-subtab-ul");
+            // find the a tags for the subnav menu
+            var subpageATag = subpageDOM.find("li a");
 
-               subpageATag.each(function(index,element){
-                  var subpageHREF = $(element).attr("href");
-                  var subpageText = $(element).text().trim();
+            subpageATag.each(function(index,element){
+                // grab the subnav menu's link
+                var subpageHREF = $(element).attr("href");
+                subpageHREF = subpageHREF.replace(/^.*\.bc\.edu/,"");
 
-                  if (loc == subpageHREF) {
-                     $(element).addClass("active");
-                     subpageDOM.show();
-                     activeLI.parent("li").removeClass("active");
-                     $("#s-lib-bc-list").append('<li><a title="'+parentText+'" href="'+parentHREF+'">'+parentText+'</a></li>');
-                     $("#s-lib-bc-list").append('<li>'+subpageText+'</li>');
-                     return;
-                  }
-               });
-            }
-         }
-      });
+                // compare the subnav menu to the page location
+                if (loc == subpageHREF) {
+                    // make sure the true active subnav menu is set as active
+                    $(element).addClass("active");
+                    // show the entire subnav menu, which is usually hidden
+                    // TODO: check if nav menu is horizontal or vertical
+                    //    vertical nav menus shouldn't have the subnav menu always showing
+                    subpageDOM.show();
+                    // make sure the parent menu isn't set as active anymore
+                    activeLI.parent("li").removeClass("active");
+                    // edit breadcrumbs
+                    var subpageBreadcrumb = '<li id="s-lib-bc-parentpage"><a title="'+parentText+'" href="'+parentHREF+'">'+parentText+'</a></li>';
+                    $(subpageBreadcrumb).insertBefore("#s-lib-bc-list #s-lib-bc-page");
+                    return;
+                }
+            });
+        }
+    }
+
+    // remove href and title attributes from page-level breadcrumb when in edit-mode
+    if ($(".edit-mode") && $(".edit-mode").length > 0) {
+        $("#s-lib-bc-list #s-lib-bc-guide a").removeAttr("href title");
+    }
+});

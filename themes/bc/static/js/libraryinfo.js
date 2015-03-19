3eb20e9662578cecd1dd0cfa9768c7d9ecb53db2
Benjamin Florin <benjamin.florin@gmail.com>
2015-03-19 13:17:21 -0400

    Changed to hover-and-toggle menu.

--- themes/bc/static/js/libraryinfo.js
+++ themes/bc/static/js/libraryinfo.js
@@ -14,30 +14,36 @@
           $(topParent).find(".item_pane").not($("#" + className).show()).hide();
         };
 
+        /* changes the library display pane */
+        var changeLibrary = function(){
+              /* get list of class values and split them into an array */
+              var myClasses = $(this).attr("class").split(/\s+/);
+              var myClassPane;
+
+              /* in the case there are multiple class values for each li element */
+              if (myClasses && myClasses.length > 1) {
+                  /* go through each and get the one class name that
+                   // matches "_pane", which is what we know to look for */
+                  $.each(myClasses, function(i,e){
+                      if (e.indexOf("_pane") != -1) {
+                          myClassPane = myClasses[i];
+                          return false;
+                      }
+                  });
+              } else {
+                  myClassPane = myClasses[0];
+              }
+              /* add selected class and toggle menu contents div */
+              updateMenu(myClassPane);
+          }
+
         /* set up which menu to show as default */
         updateMenu('oneill_pane');
         updateMenu('chat_pane');
 
-        /* when menu li element gets clicked */
-        $("ul.item-list li").click(function(){
-          /* get list of class values and split them into an array */
-          var myClasses = $(this).attr("class").split(/\s+/);
-          var myClassPane;
 
-          /* in the case there are multiple class values for each li element */
-          if (myClasses && myClasses.length > 1) {
-            /* go through each and get the one class name that
-            // matches "_pane", which is what we know to look for */
-            $.each(myClasses, function(i,e){
-              if (e.indexOf("_pane") != -1) {
-                myClassPane = myClasses[i];
-                return false;
-              }
-            });
-          } else {
-            myClassPane = myClasses[0];
-          }
-          /* add selected class and toggle menu contents div */
-          updateMenu(myClassPane);
-        });
+
+        /* when menu li element gets clicked */
+        $("ul.item-list li").hoverIntent(changeLibrary);
+        $("ul.item-list li").focusin(changeLibrary);
       });

0dac086009215fde508b1532acaa7ea1237eda8b
Benjamin Florin <benjamin.florin@gmail.com>
2015-03-31 17:05:47 -0400

    Made research guide dropdown
    commit 8ef6d8804375eb620659d1d0479894e931f5a9d8
    Author: Benjamin Florin <benjamin.florin@gmail.com>
    Date:   Tue Mar 31 17:04:56 2015 -0400
    
        Finished subject list
    
    commit a32c794a454ed3b14d66847c9fb5a1ab508d0c69
    Author: Benjamin Florin <benjamin.florin@gmail.com>
    Date:   Tue Mar 31 10:39:05 2015 -0400
    
        Made basic research guide dropdown

--- themes/bc/static/js/bc-home-chosen.js
+++ themes/bc/static/js/bc-home-chosen.js
@@ -1,25 +1,25 @@
 $('document').ready(function () {
+
+    $('#subject-guides').chosen({
+        width: "100%",
+        search_contains: true
+    }).change(function (event, params) {
+        window.location = 'http://libguides.bc.edu/' + params.selected;
+    });
+
     $.getJSON("//libdev.bc.edu/db/resources?callback=?", function (msg) {
         var options = '';
+        var $by_title = $('#dbs-by-title');
         jQuery.each(msg, function (index, value) {
             var url = 'http://databases.bc.edu/V?func=native-link&resource=' + value.number;
             options = options + '<option value="' + url + '">' + value.short_name + '</option>'
         });
-        $('#dbs-by-title').append(options);
 
-        $('#dbs-by-title').chosen({
+        $by_title.append(options).chosen({
             width: "100%",
             search_contains: true
         }).change(function (event, params) {
             window.location = params.selected;
-        });
-
-        $('#dbs-by-subject').chosen({
-            width: "100%",
-            search_contains: true
-        }).change(function (event, params) {
-            var urlPath = jQuery(this).val();
-            window.location.href = "http://libguides.bc.edu/subjects/" + urlPath + "?t=db";
         });
     });
 });

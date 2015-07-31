1643bac703e6766065ae9bc069c5aac2670091c4
Benjamin Florin <benjamin.florin@gmail.com>
2015-07-31 11:14:31 -0400

    Use correct search service

--- themes/bc/static/bcbento/js/bclib-typeahead.js
+++ themes/bc/static/bcbento/js/bclib-typeahead.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     engine = new Bloodhound({
         name: 'holmes-typeahead',
         remote: {
-            url: 'http://library.bc.edu/search-services/typeahead?any=%QUERY&callback=?',
+            url: '/search-services/typeahead?any=%QUERY&callback=?',
             rateLimitWait: 100,
             rateLimitBy: 'throttle'
         },

44e116b3297f259137857f7e16a13e985a429592
Benjamin Florin <benjamin.florin@gmail.com>
2019-03-29 16:46:09 -0400

    First working new Bento version

---
+++ themes/bc/static/bcbento/precache-manifest.1a185668d0b39dec23bb79a65fdc4fdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b003cc9ddcea43904803fa00490591c",
+    "url": "/theme/bcbento/static/media/video-tape.7b003cc9.svg"
+  },
+  {
+    "revision": "cb59635b2c859b7cfd79156ee319cd79",
+    "url": "/theme/bcbento/static/media/mono-dvd-mount.cb59635b.svg"
+  },
+  {
+    "revision": "8c8df489b30ccf12f5c390b2d1dbfbba",
+    "url": "/theme/bcbento/static/media/blank-screen.8c8df489.svg"
+  },
+  {
+    "revision": "92e50514b733871a6ed8",
+    "url": "/theme/bcbento/static/js/runtime~main.c8c18500.js"
+  },
+  {
+    "revision": "f5e111bfae9831548703",
+    "url": "/theme/bcbento/static/js/main.8d7e30eb.chunk.js"
+  },
+  {
+    "revision": "f795a2deeb4a797a16b7",
+    "url": "/theme/bcbento/static/js/2.394f3979.chunk.js"
+  },
+  {
+    "revision": "99efbce8624ea8efab00c3fdcee39831",
+    "url": "/theme/bcbento/index.html"
+  }
+];

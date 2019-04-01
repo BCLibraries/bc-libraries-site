8c6b5b4bb77b11c48d919f1afa8c800f38108ce3
Benjamin Florin <benjamin.florin@gmail.com>
2019-04-01 15:51:08 -0400

    Style to match

--- themes/bc/static/bcbento/precache-manifest.70b2b6efd2d0bd6bdcd36d719d4db8e8.js
+++ themes/bc/static/bcbento/precache-manifest.70b2b6efd2d0bd6bdcd36d719d4db8e8.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "7b003cc9ddcea43904803fa00490591c",
-    "url": "/theme/bcbento/static/media/video-tape.7b003cc9.svg"
+    "revision": "38e1c445f7cb7e03c61c14ae5794944c",
+    "url": "/theme/bcbento/static/media/video-tape.38e1c445.svg"
   },
   {
     "revision": "cb59635b2c859b7cfd79156ee319cd79",
@@ -16,15 +16,15 @@ self.__precacheManifest = [
     "url": "/theme/bcbento/static/js/runtime~main.c8c18500.js"
   },
   {
-    "revision": "28045148719be93f85af",
-    "url": "/theme/bcbento/static/js/main.72f285d3.chunk.js"
+    "revision": "1e0d0a20183c88db3157",
+    "url": "/theme/bcbento/static/js/main.278963f9.chunk.js"
   },
   {
-    "revision": "242b832b0ed06b3870ae",
-    "url": "/theme/bcbento/static/js/2.150fa27e.chunk.js"
+    "revision": "de48514844311fc4812c",
+    "url": "/theme/bcbento/static/js/2.0a72dbf0.chunk.js"
   },
   {
-    "revision": "e581dd55dba1a0c676d24ddaed0bc400",
+    "revision": "4975d8a918c8dd88e41cc2fb929a0324",
     "url": "/theme/bcbento/index.html"
   }
 ];

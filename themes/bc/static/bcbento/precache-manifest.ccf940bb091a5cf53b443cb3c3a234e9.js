de988b95524d1bae2273a12c0d8afea61d7d2813
Benjamin Florin <benjamin.florin@gmail.com>
2019-08-21 16:40:05 -0400

    Add link to video tab

--- themes/bc/static/bcbento/precache-manifest.ccf940bb091a5cf53b443cb3c3a234e9.js
+++ themes/bc/static/bcbento/precache-manifest.ccf940bb091a5cf53b443cb3c3a234e9.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "../theme/bcbento/static/js/runtime~main.1cf1c718.js"
   },
   {
-    "revision": "45162608b92a0019b666",
-    "url": "../theme/bcbento/static/js/main.fbeacde4.chunk.js"
+    "revision": "300d87b449bab9577fe0",
+    "url": "../theme/bcbento/static/js/main.6999d9b8.chunk.js"
   },
   {
     "revision": "db69efb47a54005a7e5a",
     "url": "../theme/bcbento/static/js/2.604408d5.chunk.js"
   },
   {
-    "revision": "d49744fa190d2db7bf2a3cf97ee0f940",
+    "revision": "b7c28d0e6b9bc47716f102e9f037eadd",
     "url": "../theme/bcbento/index.html"
   }
 ];

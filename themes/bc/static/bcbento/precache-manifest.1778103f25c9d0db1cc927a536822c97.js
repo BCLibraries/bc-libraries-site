1dc3b73fded54c200b2b682fd5bc2bbd2f2dd70f
Benjamin Florin <benjamin.florin@gmail.com>
2019-10-03 09:02:18 -0400

    Add context to PCI searches

--- themes/bc/static/bcbento/precache-manifest.1778103f25c9d0db1cc927a536822c97.js
+++ themes/bc/static/bcbento/precache-manifest.1778103f25c9d0db1cc927a536822c97.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "../theme/bcbento/static/js/runtime~main.1cf1c718.js"
   },
   {
-    "revision": "31dc911a3b6bb3d486d1",
-    "url": "../theme/bcbento/static/js/main.3bdf7233.chunk.js"
+    "revision": "3a61d907eb02e460813f",
+    "url": "../theme/bcbento/static/js/main.78fc499d.chunk.js"
   },
   {
     "revision": "5f77e06126d4a1d81054",
     "url": "../theme/bcbento/static/js/2.36c591c8.chunk.js"
   },
   {
-    "revision": "051371ca4dccef991fb488ad6f1d7606",
+    "revision": "1b8663a3f023e61c8c1bc658db24833c",
     "url": "../theme/bcbento/index.html"
   }
 ];
--- themes/bc/static/bcbento/service-worker.js
+++ themes/bc/static/bcbento/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "../theme/bcbento/precache-manifest.37b96a350a0d47310bc96822329838b1.js"
+  "../theme/bcbento/precache-manifest.1778103f25c9d0db1cc927a536822c97.js"
 );
 
 workbox.clientsClaim();
